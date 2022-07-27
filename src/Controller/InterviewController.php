8447834aaf36185ebbfeb8528517ef00bb8e31b8
Issam KHADIRI <khadiri.issam@gmail.com>
2022-07-27 22:54:39 +0100

    [Feature] Add the DQL date() function

---
+++ src/Controller/InterviewController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\InterviewRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+class InterviewController
+{
+    public function __construct(private InterviewRepository $repository)
+    {
+    }
+
+    #[Route(path: "/", name: "app_homepage")]
+    public function index(): Response
+    {
+        $dailyInterviews = $this->repository->listDailyInterviews();
+
+        dd($dailyInterviews);
+
+        return new Response();
+    }
+}
